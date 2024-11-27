a0e8e673035ed747a0e171614a413f0863c11c51
Yuuki Udagawa <s23C1022VL@s.chibakoudai.jp>
2024-11-27 16:27:49 +0900

    Ok

--- test.bash
+++ test.bash
@@ -2,26 +2,32 @@
 #SPDX-FileCopyrightText: 2024 Yuuki Udagawa
 
 ng () {
-	echo ${1}行目が違うよ
-	res=1
+    echo "${1}行目が違うよ"
+    res=1
 }
 
 res=0
 
-out=$(seq 5 | ./plus)
-[ "${out}" = 15 ] || ng "$LINENO"
 
-　 
-### STRANGE INPUT ###
-out=$(echo あ | ./plus)           
-[ "$?" = 1 ]      || ng "$LINENO" 
-[ "${out}" = "" ] || ng "$LINENO" 
-
-out=$(echo | ./plus)              
-[ "$?" = 1 ]      || ng "$LINENO" 
-[ "${out}" = "" ] || ng "$LINENO"
+out=$(echo This is a pen. | ./counter)
+[ "${out}" = "入力された文字数と単語数は14文字と4単語です" ] || ng "$LINENO"
 
 
-[ "${res}" = 0 ] && echo OK
+out=$(echo あ | ./counter)
+[ "${out}" = "入力された文字数と単語数は1文字と1単語です" ] || ng "$LINENO"
+
+out=$(echo 1 | ./counter)
+[ "${out}" = "入力された文字数と単語数は1文字と1単語です" ] || ng "$LINENO"
+
+out=$(echo ! | ./counter)
+[ "${out}" = "入力された文字数と単語数は1文字と1単語です" ] || ng "$LINENO"
+
+out=$(echo | ./counter)
+[ "${out}" = "入力なし" ] || ng "$LINENO"
+
+
+
+[ "$res" = 0 ] && echo "OK"
 
 exit $res
+
