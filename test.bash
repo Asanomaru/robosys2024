1e79bc11b138a5fa06f86eb0cbae73c8eb36cfd8
Yuuki Udagawa <s23C1022VL@s.chibakoudai.jp>
2024-11-29 19:46:48 +0900

    Change command test

--- test.bash
+++ test.bash
@@ -9,7 +9,7 @@ ng () {
 
 res=0
 
-
+#正常な入力
 out=$(echo This is a pen. | ./counter)
 [ "${out}" = "入力された文字数は14文字、単語数は4単語です
 使われた単語
@@ -18,16 +18,32 @@ out=$(echo This is a pen. | ./counter)
 3語目：a
 4語目：pen." ] || ng "$LINENO"
 
-
-out=$(echo あ | ./counter)
-[ "${out}" = "入力された文字数は1文字、単語数は1単語です
+out=$(echo 20 years old | ./counter)
+[ "${out}" = "入力された文字数は12文字、単語数は3単語です
 使われた単語
-1語目：あ" ] || ng "$LINENO"
+1語目：20
+2語目：years
+3語目：old" ] || ng "$LINENO"
 
 out=$(echo | ./counter)
 [ "${out}" = "入力なし" ] || ng "$LINENO"
 
+#異常な入力
+out=$(echo 12 | ./counter)
+[ "$?" = 1 ]      || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
 
+out=$(echo あ | ./counter)
+[ "$?" = 1 ]      || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
+out=$(echo 漢字 | ./counter)
+[ "$?" = 1 ]      || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
+out=$(echo ! | ./counter)
+[ "$?" = 1 ]      || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
 
 [ "$res" = 0 ] && echo "OK"
 
